194fa2354f3cb6a66ce25d769e794fd54a9bff72
Santiago Sanchez <santiago.snchez@gmail.com>
2025-07-03 04:00:47 +0000

    add: includes functions to cluster, filter variants by distance and produce a BED file

--- jacksonii_analyses/clustering.py
+++ jacksonii_analyses/clustering.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import fclusterdata
 
 
 def read_populations(pop_file: str) -> pd.DataFrame:
@@ -26,3 +27,65 @@ def calculate_pca(
         pcs_df["populations"] = pops.loc[pcs_df.index, "populations_clean"].values
 
     return pcs_df
+
+
+def cluster_variants_by_distance(
+    pos_df: pd.DataFrame, distance_threshold: float = 10000
+) -> pd.DataFrame:
+    clusters = []
+
+    for chrom, group in pos_df.groupby("chrom"):
+        if len(group) > 2:
+            positions = group["pos"].values.reshape(-1, 1)
+            # Assign cluster labels based on distance threshold
+            labels = fclusterdata(
+                positions, t=distance_threshold, criterion="distance", method="single"
+            )
+            group = group.copy()
+            group["cluster"] = labels
+            group["cluster_id"] = (
+                group["chrom"].astype(str) + "_" + group["cluster"].astype(str)
+            )
+            clusters.append(group)
+
+    clusters_df = pd.concat(clusters).reset_index(drop=True)
+
+    return clusters_df
+
+
+def cluster_data_to_intervals(clust_df: pd.DataFrame) -> pd.DataFrame:
+    intervals = []
+
+    for _, group in clust_df.groupby("cluster_id"):
+        start = group["pos"].min()
+        end = group["pos"].max()
+
+        intervals.append(
+            {
+                "chrom": group["chrom"].iloc[0],
+                "start": start,
+                "end": end,
+                "cluster_id": group["cluster_id"].iloc[0],
+                "size": end - start + 1,
+                "n_variants": len(group),
+            }
+        )
+
+    intervals_df = pd.DataFrame(intervals)
+    intervals_df = intervals_df.sort_values(
+        by=["chrom", "start", "end"], ascending=[True, True, True]
+    ).reset_index(drop=True)
+
+    return intervals_df
+
+
+def distance_for_group(df: pd.DataFrame) -> pd.Series:
+    if len(df) < 2:
+        df["distance"] = 0
+        return df
+
+    shifted_start = df["start"].copy()
+    shifted_start.iloc[0] = df["end"].iloc[0]
+    df["distance"] = df["end"] - shifted_start
+
+    return df
